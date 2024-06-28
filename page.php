c8899e474bc6588e58d438b7fc02b00c5a2ab165
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-28 14:25:27 +0100

    Update page.php

--- page.php
+++ page.php
@@ -2,14 +2,6 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
-if (strpos($_SERVER['REQUEST_URI'], '/services/') !== false) {
-    add_action('wp_head', function() {
-        generate_pricing_table_schema();
-        generate_breadcrumb_schema();
-    });
-}
-
-
 get_header();
 
 ?>
