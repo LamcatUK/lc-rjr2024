b63bf98000a89aafc9aafca3bf2b1c12c105e972
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2024-07-08 20:09:58 +0100

    quote banner changes

--- page-templates/blocks/lc_quote_banner.php
+++ page-templates/blocks/lc_quote_banner.php
@@ -1,8 +1,8 @@
 <section class="quote_banner py-5">
     <div class="container-xl">
         <div class="text-center text-md-start">
-            <h2>Need a Quote?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <h2 class="text-green-400"><?=get_field('quote_cta_title','options')?></h2>
+            <p><?=get_field('quote_cta_content','options')?></p>
         </div>
         <img src="<?=get_stylesheet_directory_uri()?>/img/chevrons-600.svg" alt="">
         <div>
